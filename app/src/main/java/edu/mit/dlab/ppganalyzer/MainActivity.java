603a20f3e6468ba725c719155945d1ab325f924d
Samantha Fierro <sfierro@mit.edu>
2015-12-10 12:29:39 -0500

    fixed file directory

--- app/src/main/java/edu/mit/dlab/ppganalyzer/MainActivity.java
+++ app/src/main/java/edu/mit/dlab/ppganalyzer/MainActivity.java
@@ -177,9 +177,11 @@ public class MainActivity extends Activity {
             if (!recordingStarted) {
                 final Calendar c = Calendar.getInstance();
                 String filepath = Environment.getExternalStorageDirectory().getPath();
-                File file = new File(filepath, extras.getString("id"));
+                File file = new File(filepath, "PPG Analyzer");
                 if (!file.exists()) file.mkdirs();
-                File saveFile = new File(file, "-waveforms-" + c.get(Calendar.YEAR) + "_" +
+                File file2 = new File(file, extras.getString("id"));
+                if (!file2.exists()) file2.mkdirs();
+                File saveFile = new File(file2, "waveforms-" + c.get(Calendar.YEAR) + "_" +
                         (c.get(Calendar.MONTH) + 1) + "_" + c.get(Calendar.DAY_OF_MONTH));
                 Recorder.filePath = saveFile.getAbsolutePath();
                 Recorder.fileName = extras.getString("id") + "-waveforms-" + c.get(Calendar.YEAR) + "_" +
